1984d7536c0d1ce69f938808752f89a82a259e29
Prasanna <prasanna@mac.local>
2024-09-03 21:20:11 +0530

    remove authentication logic

--- food-donation-app-userservice/src/main/java/com/connectritam/fooddonation/userservice/controller/AuthControllerV01.java
+++ food-donation-app-userservice/src/main/java/com/connectritam/fooddonation/userservice/controller/AuthControllerV01.java
@@ -5,10 +5,13 @@ import java.net.URI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +22,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.connectritam.fooddonation.userservice.dto.CreateUsersDTO;
 import com.connectritam.fooddonation.userservice.mapper.UserMapper;
 import com.connectritam.fooddonation.userservice.model.Users;
+import com.connectritam.fooddonation.userservice.service.CustomUserDetailsService;
 import com.connectritam.fooddonation.userservice.service.UserService;
+import com.connectritam.fooddonation.userservice.util.JwtUtil;
 
 @RestController
 @RequestMapping("/api/v0.1/auth")
@@ -36,10 +41,16 @@ public class AuthControllerV01 {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    JwtUtil jwtUtil;
+
     @PostMapping("/signup")
     public ResponseEntity<CreateUsersDTO> createUser(@RequestBody CreateUsersDTO userDTO) {
 
-        userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        userDTO.setPassword(userDTO.getPassword()); // FIXIT encode the password
         Users createdUser = userService.createUser(userDTO);
 
         CreateUsersDTO userDTOUpdated = UserMapper.INSTANCE.toCreateUsersDTO(createdUser);
@@ -51,13 +62,13 @@ public class AuthControllerV01 {
     @PostMapping("/signin")
     public ResponseEntity<String> login(@RequestBody Users user) {
         try {
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
+            // FIX IT validate the user here
+
         } catch (BadCredentialsException e) {
             throw new RuntimeException("Incorrect username or password");
         }
 
-        return ResponseEntity.ok().body("Login successful");
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Failed to authenticate user");
 
     }
 
